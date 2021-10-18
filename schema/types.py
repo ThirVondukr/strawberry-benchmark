dabb7881b0ce1139bd9c0688ef0ad894b58147ab
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-10-18 12:59:59 +0530

    document schema types
    also convert IDs to ints

--- schema/types.py
+++ schema/types.py
@@ -9,10 +9,23 @@ from strawberry.types import Info
 
 @strawberry.type(name="Author")
 class AuthorType:
-    id: strawberry.ID
-    first_name: str
-    last_name: str
-    books_published: List[BookType]
+    id: int = strawberry.field(
+        description="""
+        The ID of the author.
+        """
+    )
+
+    first_name: str = strawberry.field(
+        description="""
+        The first name of the author.
+        """
+    )
+
+    last_name: str = strawberry.field(
+        description="""
+        The last name of the author.
+        """
+    )
 
     @strawberry.field(description="The books published by the author.")
     async def books_published(self, info: Info) -> List[BookType]:
@@ -21,16 +34,42 @@ class AuthorType:
 
 @strawberry.type(name="Tag")
 class TagType:
-    id: strawberry.ID
-    name: Optional[str]
+    id: int = strawberry.field(
+        description="""
+        The ID of the tag.
+        """
+    )
+
+    name: Optional[str] = strawberry.field(
+        description="""
+        The name of the tag.
+        """
+    )
 
 
 @strawberry.type(name="Book")
 class BookType:
-    id: strawberry.ID
-    title: str
-    isbn: str
-    published_at: datetime
+    id: int = strawberry.field(
+        description="""
+        The ID of the book.
+        """
+    )
+
+    title: str = strawberry.field(
+        description="""
+        The title of the book.
+        """
+    )
+    isbn: str = strawberry.field(
+        description="""
+        The ISBN (International Standard Book Number) of the book.
+        """
+    )
+    published_at: datetime = strawberry.field(
+        description="""
+        When the book was published.
+        """
+    )
 
     @strawberry.field(description="The authors of the book.")
     async def authors(self, info: Info) -> List[AuthorType]:
@@ -43,7 +82,11 @@ class BookType:
 
 @strawberry.type(name="Store")
 class StoreType:
-    id: strawberry.ID
+    id: int = strawberry.field(
+        description="""
+        The ID of the store.
+        """
+    )
 
     @strawberry.field(description="The books in the store.")
     async def books(self, info: Info) -> List[BookType]:
