fcb60c05d8178dd3a9c00030a598b5256aaa3956
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2021-10-18 12:36:16 +0530

    add apollo tracing extension

--- schema/__init__.py
+++ schema/__init__.py
@@ -1,6 +1,12 @@
 import strawberry
+from strawberry.extensions.tracing import ApolloTracingExtension
 
 from schema.queries import Query
 
 
-schema = strawberry.Schema(query=Query)
+schema = strawberry.Schema(
+    query=Query,
+     extensions=(
+        ApolloTracingExtension,
+    )
+)
